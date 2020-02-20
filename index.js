e0b9deacafaefa369e5f97833aae34b6588e4fe2
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-02-21 01:35:30 +0900

    initial Commit

--- index.js
+++ index.js
@@ -2,20 +2,32 @@
 import express from "express";
 // require의 역할은 일단 내 폴더에서 express파일을 찾고 없으면 node_modules에서 찾아서 가져온다.
 // 이처럼 nodejs는 작은 블록처럼 되어있고 require나 import을 사용하여 쓰게된다.
+import morgan from "morgan";
+import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+
 const app = express();
 // express를 실행해서 담음
 const PORT = 4000;
-// const handleListening(){
-//     console.log(`Listening on: http://localhost:${PORT}`)
 
-// }
+
 const handleListening = (req,res) => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req,res) => res.send("Hello from home");
-
-
 const handleProfile = (req, res) => res.send("my profile");
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));
+app.use(helmet());
+
 app.get("/", handleHome)
+// const handleConnection=(req,res,next)=>{
+//     res.send("not happening")
+// }
+// app.get("/", handleConnection, handleHome)
+
 
 app.get("/profile", handleProfile)
 
