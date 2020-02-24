72e75cf866a40d44cb02c1fb65d10eff7993d7b5
KDWCL <56942649+KDWCL@users.noreply.github.com>
2020-02-24 22:20:44 +0900

    day-7 Commit

--- Lesson4/routers/userRouter.js
+++ Lesson4/routers/userRouter.js
@@ -1,15 +1,15 @@
-import express from "express";
-import routes from "../routes";
+import express from 'express';
+import routes from '../routes';
 import {
   users,
   changePassword,
   userDetail,
   editProfile
-} from "../controllers/userController";
+} from '../controllers/userController';
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
+userRouter.get(routes.home, users);
 userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
