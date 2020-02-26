24a34ade5a3724fcd6cbd5fc98af047cbbffdcce
KDWCL <56942649+KDWCL@users.noreply.github.com>
2020-02-26 22:07:49 +0900

    day-9 Commit

--- Lesson4/app.js
+++ Lesson4/app.js
@@ -9,7 +9,7 @@ import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
 import routes from './routes';
 const app = express();
-
+// 애플리케이션 레벨 미들웨어임
 app.use(helmet());
 app.set('view engine', 'pug');
 app.use(cookieParser());
@@ -17,9 +17,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 app.use(localMiddleware);
+// 여기까지 에플리케이션 레벨 미들웨어
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
 app.use(routes.videos, videoRouter);
 
 export default app;
+
+// 정리하자
+// app.use(함수) <-- 미들웨어
+// app.use(path,함수) <-- 모든 HTTP Method 요청에 대해 실행
