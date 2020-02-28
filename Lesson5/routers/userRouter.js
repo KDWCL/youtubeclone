e8713bdbfcc2f28e5a2b62aa87039e832513040b
DongWoo Kim <dongwookim@DongWooui-MacBookPro.local>
2020-02-28 21:32:38 +0900

    day-12 Commit

--- Lesson5/routers/userRouter.js
+++ Lesson5/routers/userRouter.js
@@ -14,6 +14,6 @@ userRouter.get(routes.editProfile, editProfile);
 // 시간 많이 잡아먹은 부분. users/:id로 인식해서 안되는거였음.
 // 적는 순서를 중요하게 보자. 문제점: edit-profile로 접속이 안되어지고 userDetail로 접속되어짐
 userRouter.get(routes.changePassword, changePassword);
-userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.userDetail(), userDetail);
 
 export default userRouter;
